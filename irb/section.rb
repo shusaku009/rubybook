e368f314e281c3ac113896c311c03dce0bc90031
shusaku009 <shusaku009@gmail.com>
2022-08-01 20:25:15 +0900

    irbフォルダを追加

---
+++ irb/section.rb
@@ -0,0 +1,19 @@
+puts 2 + 3
+puts 2 * 2 * 3.14
+
+puts "Ruby"
+puts "abc" + "def"
+puts "123".to_i + "456".to_i
+
+coffe = 400
+espresso = 100
+
+puts coffe + espresso * 2
+
+total = 300 * 2
+p total
+tax = total * 0.08
+puts tax
+
+total = 300 * 2
+puts total
