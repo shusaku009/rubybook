b966063db583bd3923696b3efdad742a93544e7c
shusaku009 <shusaku009@gmail.com>
2022-08-01 19:31:47 +0900

    objectフォルダを追加

---
+++ object/blank.rb
@@ -0,0 +1,13 @@
+# 足し算の例
+puts 2 + 3
+
+# 掛け算の例
+puts 2 * 3
+
+puts 1
+=begin
+puts 2
+puts 3
+puts 4
+=end
+puts 5
---
+++ object/const.rb
@@ -0,0 +1,4 @@
+CaffeLatte = "カフェラテ"
+puts CaffeLatte
+CaffeLatte = "カフェラテ"
+puts CaffeLatte
